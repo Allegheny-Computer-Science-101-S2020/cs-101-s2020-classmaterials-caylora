d4269e8266deba521a7eb24ef8a1226ebc7ca813
caylora <caylora@allegheny.edu>
2020-01-21 10:08:03 -0500

    Finished Days.java switch code

--- Lesson2/code/Days.java
+++ Lesson2/code/Days.java
@@ -11,10 +11,24 @@ public class Days{
 			case 2:
 				System.out.println("Tuesday");
 				break;
+			case 3:
+				System.out.println("Wednesday");
+				break;
+			case 4:
+				System.out.println("Thursday");
+				break;
+			case 5:
+				System.out.println("Friday");
+				break;
+			case 6:
+				System.out.println("Saturday");
+				break;
+			case 7:
+				System.out.println("Sunday");
+				break;
 			default:
 				System.out.println("Invalid day ...");
 				break;
 		}
-		
 	}
 }
